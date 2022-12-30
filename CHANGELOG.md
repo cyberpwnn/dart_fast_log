82b26adafb9386686a72862565c5e2464a75245e
cyberpwn <danielmillst@gmail.com>
2022-12-30 10:20:50 -0500

    providers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+* Log handlers
+
 ## 1.0.1
 
 * Drop flutter requirement
--- lib/fast_log.dart
+++ lib/fast_log.dart
@@ -3,6 +3,20 @@ library fast_log;
 import 'package:colored_print/colored_print.dart';
 
 bool lDebugMode = true;
+LogHandler? lLogHandler;
+
+typedef LogHandler = void Function(LogCategory category, String log);
+
+enum LogCategory {
+  info,
+  success,
+  warning,
+  error,
+  verbose,
+  navigation,
+  actioned,
+  network
+}
 
 void _log(dynamic f, {String category = 'Info', PrintColor? color}) =>
     lDebugMode
@@ -13,23 +27,42 @@ void _log(dynamic f, {String category = 'Info', PrintColor? color}) =>
             : print("[$category]: ${f ?? 'null'}")
         : {};
 
-void info(dynamic f, {PrintColor? color}) =>
-    _log(f, category: 'Info', color: color ?? PrintColor.cyan);
+void info(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Info', color: color ?? PrintColor.cyan);
+  lLogHandler?.call(LogCategory.info, "${f ?? 'null'}");
+}
 
-void success(dynamic f, {PrintColor? color}) =>
-    _log(f, category: 'Success', color: color ?? PrintColor.green);
+void success(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Success', color: color ?? PrintColor.green);
+  lLogHandler?.call(LogCategory.success, "${f ?? 'null'}");
+}
 
-void warn(dynamic f, {PrintColor? color}) =>
-    _log(f, category: 'Warning', color: color ?? PrintColor.yellow);
+void warn(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Warning', color: color ?? PrintColor.yellow);
+  lLogHandler?.call(LogCategory.warning, "${f ?? 'null'}");
+}
 
-void error(dynamic f, {PrintColor? color}) =>
-    _log(f, category: 'Error', color: color ?? PrintColor.red);
+void error(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Error', color: color ?? PrintColor.red);
+  lLogHandler?.call(LogCategory.error, "${f ?? 'null'}");
+}
 
-void verbose(dynamic f, {PrintColor? color}) =>
-    _log(f, category: 'Verbose', color: color ?? PrintColor.white);
+void network(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Network', color: color ?? PrintColor.blue);
+  lLogHandler?.call(LogCategory.network, "${f ?? 'null'}");
+}
 
-void navigation(dynamic f, {PrintColor? color}) =>
-    _log(f, category: 'Navigation', color: color ?? PrintColor.blue);
+void verbose(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Verbose', color: color ?? PrintColor.white);
+  lLogHandler?.call(LogCategory.verbose, "${f ?? 'null'}");
+}
 
-void actioned(dynamic f, {PrintColor? color}) =>
-    _log(f, category: 'Actioned', color: color ?? PrintColor.magenta);
+void navigation(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Navigation', color: color ?? PrintColor.cyan);
+  lLogHandler?.call(LogCategory.navigation, "${f ?? 'null'}");
+}
+
+void actioned(dynamic f, {PrintColor? color}) {
+  _log(f, category: 'Actioned', color: color ?? PrintColor.magenta);
+  lLogHandler?.call(LogCategory.actioned, "${f ?? 'null'}");
+}
